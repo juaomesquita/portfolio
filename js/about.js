2ed27e02e2eaf5e84cb85d733abe5e250ca1d359
João Mesquita <juaomesquita@gmail.com>
2022-02-14 11:23:25 -0500

    Link to the api

--- js/about.js
+++ js/about.js
@@ -1,7 +1,7 @@
 const ABOUT = {
     pt: {
         buttons: ['projetos', 'contatos', 'sobre mim'],
-        intro: "My name is João Mesquita, I study web programming at the Institut Superieur d'Informatique in Montréal, QC - Canada. I currently work part-time at Provigo Marc Bougie, to help with my costs in Montréal. I study programming and web development since 2018, having started in the Information Systems course at the University Center of Patos de Minas, from where I had to leave to explore new airs in a new country. At the moment I am looking for an opportunity to put my knowledge into practice and gain experience in the area.",
+        intro: "My name is João Mesquita, I'm a fullstack developer graduated in web programming at the Institut Superieur d'Informatique in Montréal, QC - Canada. I study programming and web development since 2018, having started in the Information Systems major at the University Center of Patos de Minas, from where I had to leave to explore new airs in a new country.",
         socials: [
             {
                 name: 'LinkedIn',
@@ -17,6 +17,11 @@ const ABOUT = {
                 name: 'CV',
                 icon: 'https://juaomesquita.github.io/portfolio/images/file-code-solid.svg',
                 link: 'https://juaomesquita.github.io/portfolio/docs/cv.pdf'
+            },
+            {
+                name: 'Cats',
+                icon: 'https://juaomesquita.github.io/portfolio/images/cat-solid.svg',
+                link: 'https://juaomesquita.github.io/portfolio/cats'
             },
         ],
 
